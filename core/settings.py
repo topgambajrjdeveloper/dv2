1597e65d9dc435016299da67400d99d49b78a055
julianjr <topgambajrjdeveloper@gmail.com>
2021-11-14 14:31:04 +0100

    update version python

--- core/settings.py
+++ core/settings.py
@@ -73,11 +73,11 @@ if not DEBUG:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql',
-            'NAME': os.environ.get['NAME'],
-            'USER': os.environ.get['USER'],
-            'PASSWORD': os.environ.get['PASSWORD'],
-            'HOST': os.environ.get['HOST'],
-            'PORT': os.environ.get['PORT'],
+            'NAME': os.environ.get('NAME'),
+            'USER': os.environ.get('USER'),
+            'PASSWORD': os.environ.get('PASSWORD'),
+            'HOST': os.environ.get('HOST'),
+            'PORT': os.environ.get('PORT'),
         }
     }
     
